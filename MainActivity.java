94f456acea64eedefa2db5392f19422e38015f28
BillyGithubFan <46428381+BillyGithubFan@users.noreply.github.com>
2019-01-18 15:19:23 -0500

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -17,18 +17,20 @@ public class MainActivity extends ListActivity {
         Context context = getApplicationContext();
         Resources resources = context.getResources();
 
-
+        //get the text value from a string array "tabs_names" in strings.xml
         String[] tab_names = getResources().getStringArray(R.array.tabs_names);
         String tabname1=tab_names[0];//"My Tab 1"
         String tabname2=tab_names[1];//"My Tab 2"
         String tabname3=tab_names[2];//"My Tab 3"
         String tabname4=tab_names[3];//"My Tab 4"
         String tabname5=tab_names[4];//"My Tab 5"
-
+        //put in an string array ninja
         String[] ninja = {tabname1, tabname2, tabname3, tabname4,tabname5};
 
+        //get the images from a resources array "ninja_icons" in ninja.xml
         TypedArray ninja_icons = resources.obtainTypedArray(R.array.ninja_icons);
 
+        //call ImageAndTextAdapter.java to set list adapter, passing the layout, text and icons
         setListAdapter(new ImageAndTextAdapter(context, R.layout.secondary_layout, ninja, ninja_icons));
 
 
